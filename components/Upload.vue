e793484092d6e66ccb3e806439b7569503a98cbf
Vimal Patel <vimal@quizizz.com>
2022-01-28 13:01:24 +0530

    feat: add file inputs and allow streaming the camera

---
+++ components/Upload.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <label for="o32fjwef32">{{ type }}</label>
+    <input
+      id="o32fjwef32"
+      type="file"
+      name="imgage"
+      :accept="accept"
+      :capture="capture"
+      :multiple="multiple"
+    />
+  </div>
+</template>
+
+<script>
+const UPLOAD_TYPES = ['gallery', 'camera', 'pdf']
+export default {
+  name: 'FileUpload',
+  props: {
+    type: {
+      type: String,
+      default: UPLOAD_TYPES[0],
+      validator: (val) => UPLOAD_TYPES.includes(val),
+    },
+  },
+  computed: {
+    capture() {
+      return this.type === 'camera' ? 'environment' : undefined
+    },
+    multiple() {
+      return this.type === 'gallery'
+    },
+    accept() {
+      return this.type === 'pdf' ? 'application/pdf' : 'image/*'
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Upload type="gallery" />
+    <Upload type="camera" />
+    <Upload type="pdf" />
+
+    <center>
+      <video ref="video" width="640" height="480" autoplay></video>
+    </center>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  mounted() {
+    const video = this.$refs.video
+    const mediaStream = new MediaStream()
+    video.srcObject = mediaStream
+  },
+}
+</script>
